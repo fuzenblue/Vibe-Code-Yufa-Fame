63c1c85856f873a752222f260c899fc37d9bd766
mmeensm <ameen.treasure@gmail.com>
2026-03-14 15:57:59 +0700

    Revert "feat: add transform_taxi_data function and prompt for Airflow pipeline"
    This reverts commit e62f3a265d7d787e2174edfce3f0e78ed3de7faf.

--- dags/nyc_taxi_dag.py
+++ dags/nyc_taxi_dag.py
@@ -17,7 +17,7 @@ sys.path.insert(0, "/opt/airflow/pipelines/nyc-taxi-trips-pipeline/code")
 from ingest_taxi_data import ingest_taxi_data
 from clean_taxi_data import clean_taxi_data
 from transform_taxi_data import transform_taxi_data
-# from load_taxi_model import load_taxi_model
+from load_taxi_model import load_taxi_model
 
 default_args = {
     "owner": "vibe-coder",
@@ -53,10 +53,9 @@ with DAG(
         python_callable=transform_taxi_data,
     )
 
-    # t4_load = PythonOperator(
-    #     task_id="load_taxi_model",
-    #     python_callable=load_taxi_model,
-    # )
+    t4_load = PythonOperator(
+        task_id="load_taxi_model",
+        python_callable=load_taxi_model,
+    )
 
-    t1_ingest >> t2_clean >> t3_transform 
-    # >> t4_load
+    t1_ingest >> t2_clean >> t3_transform >> t4_load
